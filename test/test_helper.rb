f0568588ea01f96a9b801f3aa55bf822c39fbaa2
saraleecodes <sara.lee.higgins@gmail.com>
2020-03-02 13:47:40 -0800

    adds simplecov and reservation class

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -1,4 +1,8 @@
-# Add simplecov
+require 'simplecov'
+SimpleCov.start do
+	add_filter 'test/' # Tests should not be checked for coverage
+end
+
 require "minitest"
 require "minitest/autorun"
 require "minitest/reporters"
@@ -6,3 +10,6 @@ require "minitest/reporters"
 Minitest::Reporters.use! Minitest::Reporters::SpecReporter.new
 
 # require_relative your lib files here!
+require_relative '../lib/reservation'
+
+# In other test files, we will require_relative 'test_helper'
