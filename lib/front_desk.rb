866a9d2d2f77eda9c7fe5a3a53ed701039a5c0f6
saraleecodes <sara.lee.higgins@gmail.com>
2020-03-02 15:56:51 -0800

    adds reserve_room method and tests, refactors FrontDesk instantiation test for readability

--- lib/front_desk.rb
+++ lib/front_desk.rb
@@ -1,12 +1,22 @@
+require_relative 'reservation'
+
 module Hotel
 	class FrontDesk
 		attr_reader :rooms, :reservations
 
 		def initialize
 			@rooms = (1..20).to_a
+			@reservations = []
 		end
 
-		# I can access the list of all of the rooms in the hotel
+		def reserve_room(start_date, end_date)
+			return Hotel::Reservation.new(
+				id: @reservations.length + 1,
+				room: nil, # placeholder
+				start_date: start_date,
+				end_date: end_date
+			)
+		end
 
 		# I access the list of reservations for a specified room and a given date range
 		
--- test/front_desk_test.rb
+++ test/front_desk_test.rb
@@ -1,19 +1,29 @@
 require_relative 'test_helper'
 
 describe "FrontDesk class" do
-	describe "FrontDesk instantiation" do
-		before do 
-			@front_desk = Hotel::FrontDesk.new
-		end
+	before do 
+		@front_desk = Hotel::FrontDesk.new
+	end
 
+	describe "FrontDesk instantiation" do
 		it "is an instance of FrontDesk" do
 			expect(@front_desk).must_be_kind_of Hotel::FrontDesk
 		end
 
 		it "can access the list of all the rooms in the hotel" do
-			expect(@front_desk.rooms).must_be_kind_of Array
-			expect(@front_desk.rooms.first).must_equal 1
-			expect(@front_desk.rooms.last).must_equal 20
+			rooms = @front_desk.rooms
+			expect(rooms).must_be_kind_of Array
+			expect(rooms.first).must_equal 1
+			expect(rooms.last).must_equal 20
+		end
+	end
+
+	describe "#reserve_room" do
+		it "can reserve a room given a start date and an end date" do
+			start_date = Date.new(2020,3,2)
+			end_date = Date.new(2020,3,5)
+			reservation = @front_desk.reserve_room(start_date, end_date)
+			expect(reservation).must_be_kind_of Hotel::Reservation
 		end
 	end
 end
