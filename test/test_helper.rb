aaee32a25c3b8c31433402446cf114beebdd0ada
saraleecodes <sara.lee.higgins@gmail.com>
2020-03-03 10:05:32 -0800

    removes skip_until tests

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -3,9 +3,10 @@ SimpleCov.start do
 	add_filter 'test/' # Tests should not be checked for coverage
 end
 
-require "minitest"
-require "minitest/autorun"
-require "minitest/reporters"
+require 'minitest'
+require 'minitest/autorun'
+require 'minitest/reporters'
+require 'minitest/skip_dsl'
 
 Minitest::Reporters.use! Minitest::Reporters::SpecReporter.new
 
