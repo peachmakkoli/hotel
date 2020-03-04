3b79d8260928f6c87b8999ae99c4284cdbd7b9ed
saraleecodes <sara.lee.higgins@gmail.com>
2020-03-04 10:17:42 -0800

    refactors #reservation_by_room method for efficiency

--- lib/front_desk.rb
+++ lib/front_desk.rb
@@ -25,11 +25,18 @@ module Hotel
 		end
 
 		def reservations_by_room(room, start_date, end_date)
-			return @reservations.select { |reservation| reservation.room == room && reservation.date_range.include?(start_date) || reservation.date_range.include?(end_date) } 
+			# return @reservations.select { |reservation| reservation.room == room && reservation.date_range.include?(start_date) || reservation.date_range.include?(end_date) }
+			return @reservations.select { |reservation| reservation.room == room && reservation.start_date >= start_date && reservation.start_date <= end_date }
 		end
 
 		def reservations_by_date(date)
 			return @reservations.select { |reservation| reservation.start_date == date }
 		end
+
+		# I can view a list of rooms that are not reserved for a given date range, so that I can see all available rooms for that day
+
+		# I can make a reservation of a room for a given date range, and that room will not be part of any other reservation overlapping that date range
+
+		# I want an exception raised if I try to reserve a room during a date range when all rooms are reserved, so that I cannot make two reservations for the same room that overlap by date
 	end
 end
