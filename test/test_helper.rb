8adbae046fb7223f78ddec7b9613a9a65f38342e
saraleecodes <sara.lee.higgins@gmail.com>
2020-03-06 16:23:34 -0800

    adds daterange.rb to libs list

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -13,5 +13,6 @@ Minitest::Reporters.use! Minitest::Reporters::SpecReporter.new
 # require_relative your lib files here!
 require_relative '../lib/reservation'
 require_relative '../lib/front_desk'
+require_relative '../lib/date_range'
 
 # In other test files, we will require_relative 'test_helper'
