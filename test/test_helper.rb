dc157faf721f4a03ac9fb3609554dbb10c5b7d67
saraleecodes <sara.lee.higgins@gmail.com>
2020-03-06 18:04:39 -0800

    adds block.rb to libs list

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -14,5 +14,6 @@ Minitest::Reporters.use! Minitest::Reporters::SpecReporter.new
 require_relative '../lib/reservation'
 require_relative '../lib/front_desk'
 require_relative '../lib/date_range'
+require_relative '../lib/block'
 
 # In other test files, we will require_relative 'test_helper'
