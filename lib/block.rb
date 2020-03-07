87c63d53ee1346ec7b6449ca69eb865086c9a31e
saraleecodes <sara.lee.higgins@gmail.com>
2020-03-07 14:16:33 -0800

    revises Block instantiation and tests

--- lib/block.rb
+++ lib/block.rb
@@ -1,13 +1,27 @@
 require 'date'
-require_relative 'date_range'
 
 module Hotel
-	class Block 
-		attr_reader :date_range, :rooms, :rate
+	class Block
+		attr_reader :rooms, :rate, :date_range
 
-		def initialize(start_date:, end_date:, rooms:, rate:)
-			# I can create a Hotel Block if I give a date range, collection of rooms, and a discounted room rate
+		def initialize(rooms:, rate:, start_date:, end_date:)
+			@rooms = rooms
+			@rate = rate
+			@date_range = Hotel::DateRange.new(
+				start_date: start_date, 
+				end_date: end_date
+			)
+
+			# Wave 3: I want an exception raised if I try to create a Hotel Block and at least one of the rooms is unavailable for the given date range
+			
+			# Wave 3: A block can contain a maximum of 5 rooms
+
+			# Wave 3: When a room is reserved from a block of rooms, the reservation dates will always match the date range of the block
 		end
 
+		# I can check whether a given block has any rooms available
+
+		# I can reserve a specific room from a hotel block
+		# I can only reserve that room from a hotel block for the full duration of the block
 	end
 end
--- test/block_test.rb
+++ test/block_test.rb
@@ -4,10 +4,10 @@ describe "Block class" do
 	before do 
 		rooms = (1..5).to_a
 		@block = Hotel::Block.new(
-			start_date: Date.new(2020,3,2),
-			end_date: Date.new(2020,3,5),
 			rooms: rooms,
-			rate: 150.0
+			rate: 150.0,
+			start_date: Date.new(2020,3,2),
+			end_date: Date.new(2020,3,5)
 		)
 	end
 
@@ -15,5 +15,15 @@ describe "Block class" do
 		it "is an instance of Block" do
 			expect(@block).must_be_kind_of Hotel::Block
 		end
+
+		it "is set up for specific attributes and data types" do
+      [:rooms, :rate, :date_range].each do |attribute|
+        expect(@block).must_respond_to attribute
+      end
+
+			expect(@block.rooms).must_be_kind_of Array
+			expect(@block.rate).must_be_kind_of Float
+			expect(@block.date_range).must_be_kind_of Hotel::DateRange
+    end
 	end
 end
