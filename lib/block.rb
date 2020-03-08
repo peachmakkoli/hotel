96cb9b35a7934197acdf1991050d083a84d19d67
saraleecodes <sara.lee.higgins@gmail.com>
2020-03-07 19:27:14 -0800

    adds an id attribute to the Block class, tests for instantiation

--- lib/block.rb
+++ lib/block.rb
@@ -2,9 +2,10 @@ require 'date'
 
 module Hotel
 	class Block
-		attr_reader :rooms, :rate, :date_range
+		attr_reader :id, :rooms, :rate, :date_range
 
-		def initialize(rooms:, rate:, start_date:, end_date:)
+		def initialize(id:, rooms:, rate:, start_date:, end_date:)
+			@id = id
 			@rooms = rooms
 			@rate = rate
 			@date_range = Hotel::DateRange.new(
@@ -12,9 +13,7 @@ module Hotel
 				end_date: end_date
 			)
 			
-			# Wave 3: A block can contain a maximum of 5 rooms
 			raise ArgumentError.new("Block must contain between 2 and 5 rooms!") if @rooms.length < 2 || @rooms.length > 5
-			# Wave 3: When a room is reserved from a block of rooms, the reservation dates will always match the date range of the block
 		end
 
 		# I can check whether a given block has any rooms available
--- test/block_test.rb
+++ test/block_test.rb
@@ -3,6 +3,7 @@ require_relative 'test_helper'
 describe "Block class" do
 	before do 
 		@block = Hotel::Block.new(
+			id: 1,
 			rooms: (1..5).to_a,
 			rate: 150.0,
 			start_date: Date.new(2020,3,2),
@@ -16,10 +17,11 @@ describe "Block class" do
 		end
 
 		it "is set up for specific attributes and data types" do
-      [:rooms, :rate, :date_range].each do |attribute|
+      [:id, :rooms, :rate, :date_range].each do |attribute|
         expect(@block).must_respond_to attribute
       end
 
+			expect(@block.id).must_be_kind_of Integer
 			expect(@block.rooms).must_be_kind_of Array
 			expect(@block.rate).must_be_kind_of Float
 			expect(@block.date_range).must_be_kind_of Hotel::DateRange
@@ -27,6 +29,7 @@ describe "Block class" do
 		
 		it "throws an exception if the list of rooms is greater than 5" do
 			expect{ Hotel::Block.new(
+				id: 1,
 				rooms: (1..6).to_a,
 				rate: 150.0,
 				start_date: Date.new(2020,3,2),
@@ -36,6 +39,7 @@ describe "Block class" do
 
 		it "throws an exception if the list of rooms is less than 2" do
 			expect{ Hotel::Block.new(
+				id: 1,
 				rooms: [1],
 				rate: 150.0,
 				start_date: Date.new(2020,3,2),
